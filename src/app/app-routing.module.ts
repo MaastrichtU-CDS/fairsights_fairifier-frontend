7f7c66eaf2f226cb90e37adce586ca5cb35b143d
brianfred <1639013dols@zuyd.nl>
2019-04-04 15:15:58 +0200

    Added the routes to the different components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DatabaseComponent } from './database/database.component';
+import { MappingComponent } from './mapping/mapping.component';
+import { OntologyComponent } from './ontology/ontology.component';
+import { ValidateComponent } from './validate/validate.component';
+import { EditComponent } from './edit/edit.component';
+import { ResultComponent } from './result/result.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'database', component: DatabaseComponent},
+  {path: 'mapping', component: MappingComponent},
+  {path: 'ontology', component: OntologyComponent},
+  {path: 'validate', component: ValidateComponent},
+  {path: 'edit', component: EditComponent},
+  {path: 'result', component: ResultComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
