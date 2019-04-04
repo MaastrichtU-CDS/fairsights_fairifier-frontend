e69e7c6a368aa7160934102ec27d6ff5eb9ac156
brianfred <1639013dols@zuyd.nl>
2019-04-04 15:14:20 +0200

    Added all the different components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { MappingComponent } from './mapping/mapping.component';
+import { OntologyComponent } from './ontology/ontology.component';
+import { DatabaseComponent } from './database/database.component';
+import { ValidateComponent } from './validate/validate.component';
+import { EditComponent } from './edit/edit.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MappingComponent,
+    OntologyComponent,
+    DatabaseComponent,
+    ValidateComponent,
+    EditComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
