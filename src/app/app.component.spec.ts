98a2cda59fa7f679bf782075247eb020d33d51ab
brianfred <1639013dols@zuyd.nl>
2019-04-04 14:20:24 +0200

    Deleted the default header test and e2e test, also deleted the default title test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,18 +14,6 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'fairifier-frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('fairifier-frontend');
-  });
-
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
