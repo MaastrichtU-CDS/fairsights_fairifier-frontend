cc1fa5a9c8feb1cc6fe9ea4129e0c8ae0604acd1
brianfred <1639013dols@zuyd.nl>
2019-04-15 12:55:37 +0200

    UI Databasecomponent Made

--- src/app/database/database.component.ts
+++ src/app/database/database.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../service/message.service';
 
 @Component({
   selector: 'app-database',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatabaseComponent implements OnInit {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
-  ngOnInit() {
+  addDatabase() {
+    this.messageService.changeMessage('Database Added');
   }
 
+  formatChanged() {
+    console.log("Format Changed"),
+    this.messageService.changeMessage('Format Changed') 
+  }
+
+  nameChanged(newName){
+    console.log('Name Changed'),
+    this.messageService.changeMessage('Name Changed')
+  }
+
+  urlChanged(newUrl){
+    console.log('Url Changed'),
+    this.messageService.changeMessage('Url Changed')
+  }
+
+  usernameChanged(newUsername){
+    console.log('Username Changed'),
+    this.messageService.changeMessage('Username Changed')
+  }
+
+  passwordChanged(newPassword){
+    console.log('Password Changed'),
+    this.messageService.changeMessage('Password Changed')
+  }
+
+  ngOnInit() {
+    this.messageService.changeMessage('');
+  }
 }
