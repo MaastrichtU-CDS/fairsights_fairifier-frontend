998c436bdc68c89cb8bdd55c8787e0fd0f2c67a9
brianfred <1639013dols@zuyd.nl>
2019-04-15 09:39:51 +0200

    Added mappingcomponent interface

--- src/app/mapping/mapping.component.ts
+++ src/app/mapping/mapping.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
+import { MessageService } from '../service/message.service';
 
 @Component({
   selector: 'app-mapping',
@@ -7,9 +8,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MappingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
+
+  @ViewChild('labelImport')
+  labelImport: ElementRef;
+  mappingFile: any;
+  useChosenFormat = false;
 
   ngOnInit() {
+    this.messageService.changeMessage('');
+  }
+
+  downloadMapping() {
+    console.log('Download Mapping');
+    this.messageService.changeMessage('Mapping Download');
+  }
+
+  mappingFileUpload() {
+    console.log('Mapping File Upload');
+    this.messageService.changeMessage('Mapping File Upload');
+  }
+
+  mappingUpload() {
+    console.log('Mapping Upload');
+    this.messageService.changeMessage('Mapping Uploaded');
+  }
+
+  formatChanged() {
+    console.log('Format Changed');
+    this.messageService.changeMessage('Format Changed');
+  }
+
+  toggleChoosenFormat(e) {
+    this.useChosenFormat = e.target.checked;
+    this.messageService.changeMessage(this.useChosenFormat);
   }
 
 }
