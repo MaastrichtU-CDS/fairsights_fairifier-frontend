2daaef414e5684aafe7c340a32c3a35ad66cc462
brianfred <1639013dols@zuyd.nl>
2019-04-24 16:35:12 +0200

    Started on the validator component

--- src/app/validate/validate.component.ts
+++ src/app/validate/validate.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../service/message.service';
 
 @Component({
   selector: 'app-validate',
@@ -10,6 +11,19 @@ export class ValidateComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.tester();
   }
 
+  useLimit = false;
+  resultsAmount = 5;
+  sqlQuery = '';
+  testCounter = 0;
+
+  tester() {
+    while (this.testCounter < 1) 
+    {
+      this.testCounter += 1,
+      this.sqlQuery += 'ABC '
+    }
+  }
 }
