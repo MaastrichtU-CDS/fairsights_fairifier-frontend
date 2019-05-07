fdbd113077776e45a284443197c118189234d196
brianfred <1639013dols@zuyd.nl>
2019-05-07 12:11:52 +0200

    Added add database modal

---
+++ src/app/modal/add-database-modal/add-database-modal.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { DatabaseFormats } from '../../model/allDatabaseFormats'
+
+@Component({
+  selector: 'app-add-database-modal',
+  templateUrl: './add-database-modal.component.html',
+  styleUrls: ['./add-database-modal.component.css']
+})
+export class AddDatabaseModalComponent implements OnInit {
+
+    myForm: FormGroup;
+    allFormats = DatabaseFormats;
+
+  constructor(
+    public activeModal: NgbActiveModal,
+    private formBuilder: FormBuilder
+  ) { 
+      this.createForm();
+  }
+
+  ngOnInit() {
+  }
+
+  closeModal() {
+    this.activeModal.close('Modal Closed');
+  }
+
+  private createForm() {
+      this.myForm = this.formBuilder.group ({
+          format: 'CSV',
+          databasename: '',
+          password: '',
+          url: '',
+          username: ''
+      });
+  }
+
+  private submitForm() {
+      this.activeModal.close(this.myForm.value);
+  }
+
+}
