bf14fee58d2efe6e53e0eb7d1dd2936565a06d6a
brianfred <1639013dols@zuyd.nl>
2019-05-10 15:36:32 +0200

    Changed database format to also include the piece that is to be put in front of the url

--- src/app/model/allDatabaseFormats.ts
+++ src/app/model/allDatabaseFormats.ts
@@ -1,20 +1,23 @@
-export const DatabaseFormats: string[] = [
-    'CSV',
-    'DERBY',
-    'H2',
-    'HSQLDB',
-    'SQLITE',
-    'MYSQL',
-    'MARIADB',
-    'GAE',
-    'ORACLE',
-    'POSTGRESQL',
-    'HANA',
-    'JTDS',
-    'SQLSERVER',
-    'FIREBIRD',
-    'DB2',
-    'DB2_AS400',
-    'TERADATA',
-    'INFORMIX'
-];
+import { databaseFormat } from './format';
+
+
+export const DATABASEFORMATS: databaseFormat[] = [
+    { driver: 'CSV', preUrl: 'jdbc:relique:csv:'},
+    { driver: 'DERBY', preUrl: ''},
+    { driver: 'H2', preUrl: ''},
+    { driver: 'HSQLDB', preUrl: ''},
+    { driver: 'SQLITE', preUrl: ''},
+    { driver: 'MYSQL', preUrl: ''},
+    { driver: 'MARIADB', preUrl: ''},
+    { driver: 'GAE', preUrl: ''},
+    { driver: 'ORACLE', preUrl: ''},
+    { driver: 'POSTGRESQL', preUrl: ''},
+    { driver: 'HANA', preUrl: ''},
+    { driver: 'JTDS', preUrl: ''},
+    { driver: 'SQLSERVER', preUrl: ''},
+    { driver: 'FIREBIRD', preUrl: ''},
+    { driver: 'DB2', preUrl: ''},
+    { driver: 'DB2_AS400', preUrl: ''},
+    { driver: 'TERADATA', preUrl: ''},
+    { driver: 'INFORMIX', preUrl: ''}
+]
---
+++ src/app/model/format.ts
@@ -0,0 +1,4 @@
+export class databaseFormat {
+    driver: string;
+    preUrl: string;
+}
