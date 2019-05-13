d6e70ad9f9547f3e085064ebf49f2bf8464be7e7
brianfred <1639013dols@zuyd.nl>
2019-05-13 10:50:27 +0200

    Added the preUrl and made the openDatabaseModal add the preUrl to the input url and change the \ to \\

--- src/app/database/database.component.ts
+++ src/app/database/database.component.ts
@@ -3,6 +3,7 @@ import { MessageService } from '../service/message.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { AddDatabaseModalComponent } from '../modal/add-database-modal/add-database-modal.component';
 import { DatabaseService } from '../service/database.service';
+import { DATABASEFORMATS } from '../model/allDatabaseFormats';
 
 @Component({
   selector: 'app-database',
@@ -12,6 +13,7 @@ import { DatabaseService } from '../service/database.service';
 export class DatabaseComponent implements OnInit {
 
     dataSource: string;
+    driverUrl: string;
 
     constructor(
         public messageService: MessageService,
@@ -19,9 +21,19 @@ export class DatabaseComponent implements OnInit {
         private databaseService: DatabaseService
     ) { }
 
+    private getByName(nameDriver) {
+        for ( var i = 0, iLen = DATABASEFORMATS.length; i<iLen; i++) {
+            if (DATABASEFORMATS[i].driver == nameDriver) return DATABASEFORMATS[i].preUrl
+        }
+    }
+
     openDatabaseModal() {
         const modalRef = this.modalService.open(AddDatabaseModalComponent);
         modalRef.result.then((result) => {
+            result.url = result.url.replace(/\\/g, '\\\\');
+            this.driverUrl = this.getByName(result.format)
+            result.url =  this.driverUrl+ result.url
+            console.log(result.url)
             this.dataSource = '{ \"driver\": \"' + result.format + '\", \"name\": \"' +
             result.databasename + '\", \"password\": \"' + result.password + '\", \"url\": \"' +
             result.url + '\", \"username\": \"' + result.username + '\" }';
