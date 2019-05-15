18a7473057265d0b998734359ccf165bab67341d
brianfred <1639013dols@zuyd.nl>
2019-05-15 14:39:02 +0200

    Added the mappingformats

--- src/app/model/format.ts
+++ src/app/model/format.ts
@@ -8,3 +8,9 @@ export class OntologyFormat {
     shortName: string;
     extension: string;
 }
+
+export class MappingFormat {
+    fullName: string;
+    shortName: string;
+    extension: string;
+}
