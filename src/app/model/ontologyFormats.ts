a6d9733b79c5449a38c106d99f5ae4483446427b
brianfred <1639013dols@zuyd.nl>
2019-05-16 14:06:38 +0200

    Added the new ontology formats in given by johan

--- src/app/model/ontologyFormats.ts
+++ src/app/model/ontologyFormats.ts
@@ -1,11 +1,20 @@
 import { OntologyFormat } from './format';
 
 export const ONTOLOGYFORMATS: OntologyFormat[] = [
-    { fullName: 'FormaatOne', shortName: 'FOR1' , extension: '.fo1'},
-    { fullName: 'FormaatTwo', shortName: 'FOR2' , extension: '.fo2'},
-    { fullName: 'FormaatThree', shortName: 'FOR3' , extension: '.fo3'},
+    { fullName: 'Web Ontology Language', shortName: 'OWL' , extension: '.owl'},
+    { fullName: 'BinaryRDF', shortName: 'BINARY', extension: '.bdf'},
+    { fullName: 'JSON-LD', shortName: 'JSONLD', extension: '.jsonld'},
+    { fullName: 'N3', shortName: 'N3', extension: '.n3'},
+    { fullName: 'N-Quads', shortName: 'NQUADS', extension: '.nq'},
+    { fullName: 'N-Triples', shortName: 'NTRIPLES', extension: '.nt'},
+    { fullName: 'RDFa', shortName: 'RDFA', extension: '.rdfa'},
+    { fullName: 'RDF/JSON', shortName: 'RDFJSON', extension: '.rj'},
+    { fullName: 'RDF/XML', shortName: 'RDFXML', extension: '.rdf'},
+    { fullName: 'TriG', shortName: 'TRIG', extension: '.trig'},
+    { fullName: 'TriX', shortName: 'TRIX', extension: '.xml'},
+    { fullName: 'Turtle', shortName: 'TURTLE', extension: '.ttl'}
 ];
 
 export const  pickedOntologyFormat: OntologyFormat[] = [
-    { fullName: 'FormaatOne', shortName: 'FOR1' , extension: '.fo1'},
+    { fullName: 'Web Ontology Language', shortName: 'OWL' , extension: '.fo1'},
 ];
