b51c74e007bb23af47199bd2f15af64b2a278bac
brianfred <1639013dols@zuyd.nl>
2019-05-23 12:40:40 +0200

    Added a edit component

--- src/app/edit/edit.component.ts
+++ src/app/edit/edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../service/message.service';
+import { EditService } from '../service/edit.service';
 
 @Component({
   selector: 'app-edit',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+      private messageService: MessageService,
+      private editService: EditService
+  ) { }
 
   ngOnInit() {
+      this.messageService.changeMessage('')
   }
 
+  databaseName;
+  resultsAmount;
+
+  changeMappingDefinitions(newMappingDefinition) {
+      this.messageService.changeMessage('Mapping Definitions Changed'),
+      console.log('change mapping definitions'),
+      this.editService.mappingDefinitionChanged = newMappingDefinition.target.value
+      console.log(this.editService.mappingDefinitionChanged)
+  }
+
+  saveMappingDefinitions() {
+      this.messageService.changeMessage('Mapping Definitions Save')
+      console.log('save mapping definitions'),
+      this.editService.testMappingDefinitions()
+  }
+
+  testMappingDefinitions() {
+      this.messageService.changeMessage('Mapping Definitions Test'),
+      console.log('test mapping definitions'),
+      this.editService.testMappingDefinitions()
+  }
+
+  changeResultsAmount(newAmount) {
+      this.resultsAmount = newAmount.target.value;
+      console.log(this.resultsAmount)
+      this.messageService.changeMessage( 'New Amount: ' + this.resultsAmount)
+  }
+
+  changeUsedDatabase(newDatabase)  {
+      this.databaseName = newDatabase.target.value,
+      this.messageService.changeMessage('Database changed to: ' + this.databaseName),
+      console.log(this.databaseName)
+  }
 }
---
+++ src/app/service/edit.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MessageService } from '../service/message.service';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EditService {
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) { }
+  
+  mappingDefinitionChanged = '';
+
+    testMappingDefinitions() {
+        console.log('Mapping definition testing is not yet implemented')
+    }
+    saveMappingDefinitions() {
+        console.log('Mapping definitions saving is not yet implemented')   
+    }
+}
+
