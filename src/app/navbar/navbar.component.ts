bd63d54a0765882be3c53655e142d7a814ffd187
brianfred <1639013dols@zuyd.nl>
2019-06-03 15:39:02 +0200

    Added a bit of code to make sure the program used a different graphic for the active tab in the navbar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -10,6 +10,26 @@ export class NavbarComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+      this.setActive();
   }
+
+    setActive() {
+        const btnContainer = document.getElementById('navbarCollapse');
+
+        const btns = btnContainer.getElementsByClassName('nav-link');
+
+        for (let i = 0; i < btns.length; i++) {
+            btns[i].addEventListener('click', function() {
+                const current = document.getElementsByClassName('active');
+                console.log(current);
+
+                if (current.length > 0) {
+                current[0].className = current[0].className.replace(' active', '');
+                }
+
+                this.className += ' active';
+            });
+        }
+    }
 
 }
