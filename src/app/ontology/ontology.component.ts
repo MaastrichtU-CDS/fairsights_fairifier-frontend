6f4c7f6f170ad0a8898a36705f238bb2a3111d9a
brianfred <1639013dols@zuyd.nl>
2019-06-19 08:44:43 +0200

    Made a table in the ontology page for showing the loaded in ontologies

--- src/app/ontology/ontology.component.ts
+++ src/app/ontology/ontology.component.ts
@@ -22,6 +22,7 @@ export class OntologyComponent implements OnInit {
 
     ngOnInit() {
         this.messageService.changeMessage('');
+        this.ontologyService.getOntologies();
     }
 
     openOntologyModal() {
--- src/app/service/ontology.service.ts
+++ src/app/service/ontology.service.ts
@@ -8,10 +8,33 @@ import { environment } from '../../environments/environment';
 })
 export class OntologyService {
 
+    public ontologyArray = [];
+
     constructor(
         private http: HttpClient,
         private messageService: MessageService
     ) { }
+
+    getOntologies() {
+        const extraUrl = '/ontologies'
+        const url = environment.apiUrl + extraUrl
+
+        this.http.get(url)
+        .subscribe(
+            data => {
+                this.putOntologiesInArray(data)
+            },
+            error => {
+                console.log(error),
+                this.messageService.changeMessage('Error: ' + error.message)
+            }
+        )
+    }
+
+    putOntologiesInArray(currentOntologies) {
+        this.ontologyArray = currentOntologies
+        console.log(this.ontologyArray)
+    }
 
     uploadOntologyFile(ontologyFile, fileFormat, baseUri ) {
         console.log(ontologyFile),
