dc1001f0d0ce730e5dfd30b8df1ce64bc3fff38a
peter.fick <peter.fick@maastro.nl>
2019-11-07 14:48:27 +0100

    Fix some more failing tests

--- src/app/components/database/database.component.spec.ts
+++ src/app/components/database/database.component.spec.ts
@@ -1,4 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 import { DatabaseComponent } from './database.component';
 
@@ -8,6 +9,7 @@ describe('DatabaseComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ],
       declarations: [ DatabaseComponent ]
     })
     .compileComponents();
